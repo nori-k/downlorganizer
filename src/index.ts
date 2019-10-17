2a5912808f7f9305c5d073cb46b7f814f0879cdc
nori-k <norikatsu.kamiya@gmail.com>
2019-10-18 05:47:57 +0900

    baby steps!

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,50 @@
-function hello(name: string): string {
-  return `Hello, ${name}!`;
-}
+import * as chokidar from "chokidar";
+/**
+ **   function createWatcher(
+ **    paths: string | string[],
+ **    options: chokidar.WatchOptions = {}
+ **  ): Promise<chokidar.FSWatcher> {
+ **    return new Promise((resolve, reject) => {
+ **      try {
+ **        let watcher = chokidar.watch(paths, options);
+ **        watcher.once("ready", () => resolve(watcher));
+ **      } catch (err) {
+ **        reject(err);
+ **      }
+ **    });
+ **  }
+ **
+ **  let watcher = await createWatcher("~/Downloads");
+ **
+ **  chokidar.watch("~/Downloads").on("all", (event, path) => {
+ **    console.log(event, path);
+ **  });
+ */
 
-console.log(hello("TypeScript"));
+// todo: chokidarで ~/Downloads の変更を監視
+let watcher = chokidar.watch("~/Downloads", {
+  ignored: /[\\/\\\\]\./,
+  persistent: true
+});
+
+// todo: ~/Downloadsに変更があった場合ファイル拡張子に応じてmkdirやmvをして自動でファイル整理をする
+watcher.add("./data2/");
+watcher.add("./data3/tetete.txt");
+watcher.on("ready", () => {
+  console.log("ready watching ...]");
+  watcher.on("add", path => {
+    console.log("added : ${path}");
+  });
+  watcher.on("change", path => {
+    console.log("changed : ${path}");
+  });
+  watcher.on("unlink", path => {
+    console.log("unlink : ${path}");
+  });
+  watcher.on("addDir", path => {
+    console.log("addDir : ${path}");
+  });
+  watcher.on("unlinkDir", path => {
+    console.log("unlinkDir : ${path}");
+  });
+});
